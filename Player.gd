eddb66ccd5d5cdd759d23d58ab3eb0e582a216a0
ErTreK8 <ericgarciagispert@gmail.com>
2024-10-22 15:44:37 +0200

    Preparem les col·lisions.

--- Player.gd
+++ Player.gd
@@ -1,9 +1,13 @@
 extends Area2D
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
+signal hit
+
+
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
+
 
 func _process(delta):
 	var velocity = Vector2.ZERO # Vector de moviment del jugador.
@@ -30,3 +34,22 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_body_entered(body):
+	hide() # El jugador desapareix després de ser impactat.
+	emit_signal("hit")
+	# S'ha d'ajornar, ja que no podem canviar les propietats físiques en una crida de retorn de física.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+	
+	
+	
+	
+	
