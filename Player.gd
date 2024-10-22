94a39d7a0230243eb8289e0d5e178ca5c6785faa
ErTreK8 <ericgarciagispert@gmail.com>
2024-10-22 15:40:25 +0200

    Animacions jugador

--- Player.gd
+++ Player.gd
@@ -3,6 +3,7 @@ export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # Vector de moviment del jugador.
@@ -15,15 +16,15 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
-		
-		#if(velocity.y!=0):
-		#	$AnimatedSprite.play("up")
-		#else:
-		#	$AnimatedSprite.play("walk")		
-		
-		
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
 	else:
 		$AnimatedSprite.stop()
 	position += velocity * delta
